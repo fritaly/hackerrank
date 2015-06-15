cc9552e423222fd6a2ad60d6cfee4ebdbbcf4c16
Francois Ritaly <francois.ritaly@gmail.com>
2015-06-15 19:23:46 +0200

    Added solution to problem "Library Fine"

---
+++ src/main/groovy/com/github/fritaly/hackerrank/LibraryFine.groovy
@@ -0,0 +1,32 @@
+package com.github.fritaly.hackerrank
+
+class LibraryFine {
+
+	static void main(args) {
+		def reader = new BufferedReader(new InputStreamReader(System.in))
+		def (actualDay, actualMonth, actualYear) = reader.readLine().split(' ').collect { it as int }
+		def (expectedDay, expectedMonth, expectedYear) = reader.readLine().split(' ').collect { it as int }
+		
+		if (actualYear == expectedYear) {
+			if (actualMonth == expectedMonth) {
+				if (actualDay > expectedDay) {
+					println 15 * (actualDay - expectedDay)
+				} else {
+					println 0
+				}
+			} else {
+				if (actualMonth > expectedMonth) {
+					println 500 * (actualMonth - expectedMonth)
+				} else {
+					println 0
+				}
+			}
+		} else {
+			if (actualYear > expectedYear) {
+				println 10000
+			} else {
+				println 0
+			}
+		}
+	}
+}
