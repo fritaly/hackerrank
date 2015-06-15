372c19cdfbfc110994af5d0099c97dfd8c86496d
Francois Ritaly <francois.ritaly@gmail.com>
2015-06-15 20:59:55 +0200

    Added solution to problem "Angry Professor"

---
+++ src/main/groovy/com/github/fritaly/hackerrank/AngryProfessor.groovy
@@ -0,0 +1,17 @@
+package com.github.fritaly.hackerrank
+
+class AngryProfessor {
+
+	static void main(args) {
+		def reader = new BufferedReader(new InputStreamReader(System.in))
+		def numberOfTests = reader.readLine() as int
+
+		numberOfTests.times {
+			def (n, k) = reader.readLine().split(' ').collect { it as int }
+			def arrivalTimes = reader.readLine().split(' ').collect { it as int }
+			def present = arrivalTimes.findAll { it <= 0 }.size()
+			
+			println ((present < k) ? 'YES' : 'NO')
+		}
+	}
+}
