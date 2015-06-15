fb255ab5b0346b42ee65ac621437e0cc07e4a8d3
Francois Ritaly <francois.ritaly@gmail.com>
2015-06-15 22:05:12 +0200

    Added solution to problem "Cut The Sticks"

---
+++ src/main/groovy/com/github/fritaly/hackerrank/CutTheSticks.groovy
@@ -0,0 +1,28 @@
+package com.github.fritaly.hackerrank
+
+class CutTheSticks {
+
+	static void main(args) {
+		def reader = new BufferedReader(new InputStreamReader(System.in))
+		def numberOfSticks = reader.readLine() as int
+		def sticks = reader.readLine().split(' ').collect { it as int }
+		
+		while (sticks) {
+			def minLength = sticks.min()
+			
+			int count = 0
+			
+			sticks = sticks.findAll { 
+				def value = it - minLength
+			
+				if (value >= 0) {
+					count++
+				}
+				
+				value
+			}
+			
+			println count
+		}
+	}
+}
