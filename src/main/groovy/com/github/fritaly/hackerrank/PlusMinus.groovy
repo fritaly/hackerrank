f79ba3fb4bb8ab683bc92125ec189a18f397300e
Francois Ritaly <francois.ritaly@gmail.com>
2015-06-15 18:40:44 +0200

    Added solution to problem "Plus Minus"

---
+++ src/main/groovy/com/github/fritaly/hackerrank/PlusMinus.groovy
@@ -0,0 +1,26 @@
+package com.github.fritaly.hackerrank
+
+class PlusMinus {
+
+	static void main(args) {
+		def reader = new BufferedReader(new InputStreamReader(System.in))
+		def total = Integer.parseInt(reader.readLine())
+		def array = reader.readLine().split(' ').collect { it as int }
+
+		def negative = 0, positive = 0, zero = 0
+
+		array.each {
+			if (it < 0) {
+				negative++
+			} else if (it > 0) {
+				positive++
+			} else {
+				zero++
+			}
+		}
+
+		println String.format('%.3f', positive / total)
+		println String.format('%.3f', negative / total)
+		println String.format('%.3f', zero / total)
+	}
+}
