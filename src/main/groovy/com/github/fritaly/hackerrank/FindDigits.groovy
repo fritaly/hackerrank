f94780729335e6d582d88c13fc3e81ed32ce5d19
Francois Ritaly <francois.ritaly@gmail.com>
2015-06-16 13:30:10 +0200

    Added solution to problem "Find Digits"

---
+++ src/main/groovy/com/github/fritaly/hackerrank/FindDigits.groovy
@@ -0,0 +1,17 @@
+package com.github.fritaly.hackerrank
+
+class FindDigits {
+
+	static void main(args) {
+		def reader = new BufferedReader(new InputStreamReader(System.in))
+		def numberOfTests = reader.readLine() as int
+
+		numberOfTests.times {
+			def text = reader.readLine()
+			def n = text as int
+			def count = text.findAll { c -> (c != '0') && (n % (c as int) == 0) }.size()
+			
+			println count
+		}
+	}
+}
