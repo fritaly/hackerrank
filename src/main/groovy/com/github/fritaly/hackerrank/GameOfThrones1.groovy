87899982492dc9ae7ce7b02105be97804a2087b0
Francois Ritaly <francois.ritaly@gmail.com>
2015-06-16 20:07:06 +0200

    Added solution to problem "Game of Thrones - I"

---
+++ src/main/groovy/com/github/fritaly/hackerrank/GameOfThrones1.groovy
@@ -0,0 +1,19 @@
+package com.github.fritaly.hackerrank
+
+class GameOfThrones1 {
+
+	static void main(args) {
+		def reader = new BufferedReader(new InputStreamReader(System.in))
+		def string = reader.readLine()
+
+		def chars = [:]
+
+		for(c in string.toCharArray()) {
+			chars[c] = (chars[c] ?: 0) + 1
+		}
+		
+		def palindrome = chars.values().findAll { it % 2 == 1 }.size() <= 1
+		
+		println palindrome ? 'YES' : 'NO'
+	}
+}
