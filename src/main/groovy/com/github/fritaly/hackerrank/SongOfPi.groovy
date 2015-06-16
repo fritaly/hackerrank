8decafa547f948232d261c811de21687574296d3
Francois Ritaly <francois.ritaly@gmail.com>
2015-06-16 13:51:33 +0200

    Added solution to problem "Song of Pi"

---
+++ src/main/groovy/com/github/fritaly/hackerrank/SongOfPi.groovy
@@ -0,0 +1,21 @@
+package com.github.fritaly.hackerrank
+
+class SongOfPi {
+
+	static void main(args) {
+		def reader = new BufferedReader(new InputStreamReader(System.in))
+		def numberOfTests = reader.readLine() as int
+		def pi = '314159265358979323846'
+		
+		numberOfTests.times {
+			def words = reader.readLine().split(' ')
+			def value = words.collect { it.length() as String }.join()
+			
+			if (pi.startsWith(value) || value.startsWith(pi)) {
+				println "It's a pi song."
+			} else {
+				println "It's not a pi song."
+			}
+		}
+	}
+}
