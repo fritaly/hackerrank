844d0251823abf354648f2c60319aa915c6bf3a2
Francois Ritaly <francois.ritaly@gmail.com>
2015-06-16 19:54:28 +0200

    Added solution to problem "Alternating Characters"

---
+++ src/main/groovy/com/github/fritaly/hackerrank/AlternatingCharacters.groovy
@@ -0,0 +1,22 @@
+package com.github.fritaly.hackerrank
+
+class AlternatingCharacters {
+
+	static void main(args) {
+		def reader = new BufferedReader(new InputStreamReader(System.in))
+		def numberOfTests = reader.readLine() as int
+		
+		numberOfTests.times {
+			def string = reader.readLine()
+			def count = 0
+			
+			for(int i=0; i< string.length() - 1; i++) {
+				if (string.charAt(i) == string.charAt(i+1)) {
+					count++
+				}
+			}
+			
+			println count
+		}
+	}
+}
