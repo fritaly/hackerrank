ee78bfba1a1c5a790d80008eb118cf741a64493c
Francois Ritaly <francois.ritaly@gmail.com>
2015-06-20 09:40:52 +0200

    Added solution to problem "Make it Anagram"

---
+++ src/main/groovy/com/github/fritaly/hackerrank/MakeItAnagram.groovy
@@ -0,0 +1,28 @@
+package com.github.fritaly.hackerrank
+
+class MakeItAnagram {
+
+	static void main(args) {
+		def reader = new BufferedReader(new InputStreamReader(System.in))
+		def s1 = reader.readLine()
+		def s2 = reader.readLine()
+		
+		def chars1 = [:], chars2 = [:]
+		
+		s1.each { c ->
+			chars1[c] = (chars1[c] ?: 0) + 1
+		}
+		s2.each { c ->
+			chars2[c] = (chars2[c] ?: 0) + 1
+		}
+		
+		def chars = (s1+s2).toCharArray().toList().unique().sort().collect { it as String }
+		def result = 0
+		
+		chars.each { c ->
+			result += Math.abs((chars1[c] ?: 0) - (chars2[c] ?: 0))
+		}
+		
+		println result
+	}
+}
